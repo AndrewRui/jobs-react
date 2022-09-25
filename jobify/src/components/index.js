7bb7618ea3bd63560f138c6e7296888502deefff
unknown <arui042398@gmail.com>
2022-09-26 00:09:49 +0800

    first commit

---
+++ jobify/src/components/index.js
@@ -0,0 +1,29 @@
+import Alert from "./Alert";
+import FormRow from "./FormRow";
+import Logo from "./Logo";
+import Navbar from "./Navbar";
+import SmallSidebar from "./SmallSidebar";
+import BigSidebar from "./BigSidebar";
+import FormRowSelect from "./FormRowSelect";
+import SearchContainer from "./SearchContainer";
+import JobsContainer from "./JobsContainer";
+import StatsContainer from "./StatsContainer";
+import StatItem from "./StatItem";
+import ChartsContainer from "./ChartsContainer";
+import Loading from "./Loading";
+
+export {
+  Logo,
+  FormRow,
+  Alert,
+  Navbar,
+  SmallSidebar,
+  BigSidebar,
+  FormRowSelect,
+  SearchContainer,
+  JobsContainer,
+  StatsContainer,
+  StatItem,
+  ChartsContainer,
+  Loading,
+};
---
+++ jobify/src/pages/Landing.js
@@ -0,0 +1,35 @@
+import logo from "../assets/images/logo.svg";
+import main from "../assets/images/main.svg";
+import Wrapper from "../assets/wrappers/LandingPage";
+import { Logo } from "../components";
+import { Link } from "react-router-dom";
+
+const Landing = () => {
+  return (
+    <Wrapper>
+      <nav>
+        <Logo />
+      </nav>
+      <div className="container page">
+        {/* info */}
+        <div className="info">
+          <h1>
+            job <span>tracking</span> app
+          </h1>
+          <p>
+            I'm baby dIY locavore hashtag food truck cardigan edison bulb
+            pour-over hoodie aesthetic. Church-key lyft butcher tattooed
+            disrupt, godard vinyl XOXO Brooklyn chicharrones authentic vice
+            whatever taxidermy.
+          </p>
+          <Link to="/register" className="btn btn-hero">
+            Login/Register
+          </Link>
+        </div>
+        <img src={main} alt="job hunt" className="img main-img" />
+      </div>
+    </Wrapper>
+  );
+};
+
+export default Landing;
